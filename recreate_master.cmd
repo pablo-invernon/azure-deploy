a2f8462f5b8fad4c4d65b64cc75fbe69f0950126
Pablo Invernón Moreno <pablo_invernon@inetum.world>
2021-02-22 01:48:39 +0100

    reconfigurado el firewall para el acceso a nfs desde el cluster

--- recreate_master.cmd
+++ recreate_master.cmd
@@ -6,4 +6,7 @@ VBoxManage clonevm centostemplate --basefolder c:\desarrollo\vbox --mode=all --n
 VBoxManage startvm %VM_NAME%  --type headless
 
 
+@echo ""
+@echo ""
+@echo Run:
 @echo ansible-playbook -i hosts -l lab-new-host.local --extra-vars="target_hostname=%VM_NAME%.local" onebyone-base-config.yaml
